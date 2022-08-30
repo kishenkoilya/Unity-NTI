bbc3905ce5882f8fd96ec9d50ef88aa3f16d090e
kishenkoilya <illad08@gmail.com>
2022-08-30 19:20:57 +0300

    Revert "Dynamic pathfind + objects of jump pads and finish"
    This reverts commit c933f1a64e72dbeee91123525eb513bb31542b31.

--- Task 1.14/Assets/Scripts/NavigationScript.cs
+++ Task 1.14/Assets/Scripts/NavigationScript.cs
@@ -6,18 +6,11 @@ using UnityEngine.AI;
 public class NavigationScript : MonoBehaviour
 {
     [SerializeField] private Transform Goal;
-    private NavMeshAgent Navi;
     // Start is called before the first frame update
     void Start()
     {
-        Navi = GetComponent<NavMeshAgent>();
-    }
-
-    /// <summary>
-    /// Update is called every frame, if the MonoBehaviour is enabled.
-    /// </summary>
-    void Update()
-    {
+        NavMeshAgent Navi = GetComponent<NavMeshAgent>();
         Navi.destination = Goal.position;
     }
+
 }
