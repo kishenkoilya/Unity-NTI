9eb1a3844e94327a2fc88b305bc6a5222cbf4bdd
kishenkoilya <illad08@gmail.com>
2022-10-09 12:29:03 +0300

    Added deactivation timeout for objects returning to pool

--- Snake VS Blocks clone/Assets/Scripts/ObjectsPool.cs
+++ Snake VS Blocks clone/Assets/Scripts/ObjectsPool.cs
@@ -6,6 +6,8 @@ public class ObjectsPool : MonoBehaviour
 {
     [SerializeField] private List<GameObject> objects = new List<GameObject>();
     [SerializeField] private Factory objectFactory;
+    [SerializeField] private float deactivationTimeout = 2;
+    private List<(int, float)> objectDeactivationTimeouts = new List<(int, float)>();
     // Start is called before the first frame update
     void Awake()
     {
@@ -21,6 +23,7 @@ public class ObjectsPool : MonoBehaviour
         obj.transform.SetParent(gameObject.transform);
         obj.transform.localPosition = Vector3.zero;
         objects.Add(obj);
+        objectDeactivationTimeouts.Add((gameObject.transform.childCount - 1, deactivationTimeout));
     }
 
     public GameObject GetObject()
@@ -35,5 +38,18 @@ public class ObjectsPool : MonoBehaviour
             return obj;
         }
         return objectFactory.InstantiateObject();
+    }
+
+    private void Update() {
+        for (int i = 0; i < objectDeactivationTimeouts.Count; i++)
+        {
+            objectDeactivationTimeouts[i] = (objectDeactivationTimeouts[i].Item1, objectDeactivationTimeouts[i].Item2 - Time.deltaTime);
+            if (objectDeactivationTimeouts[i].Item2 <= 0)
+            {
+                transform.GetChild(objectDeactivationTimeouts[i].Item1).gameObject.SetActive(false);
+                objectDeactivationTimeouts.RemoveAt(i);
+                i--;
+            }  
+        }
     }
 }
